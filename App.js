2d79ae21df287cb0d176a76485c5036287bba987
abhikam395 <--add>
2021-04-15 10:28:40 +0530

    react-navigation setup

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import SpashScreen from './src/screens/SpashScreen';  
+const Stack = createStackNavigator();
+  
+function App() {
+    return (
+        <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Splash" component={SpashScreen} />
+        </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  
+export default App;
---
+++ src/screens/SpashScreen.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+export default class SpashScreen extends Component{
+    render(){
+        return (
+           <View>
+               <Text>asdf</Text>
+           </View>
+        );
+    }
+}
